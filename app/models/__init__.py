568c8465520b65193f6a3e906607b03f4810d52c
Maaz <m.maazansari108@gmail.com>
2025-10-18 12:27:16 +0530

    changes in init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,10 @@ from app.models.base_mixin import *
 from app.models.blog import *
 from app.models.event import *
 from app.models.enquiry import *
+
+
+# from . import user
+# from . import base_mixin
+# from . import blog
+# from . import event
+# from . import enquiry
